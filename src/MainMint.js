523fabf666ee02542f9e1bc73059118970f0fcb2
KBPsystem777 <koleen.bp@outlook.com>
2022-06-11 22:21:55 +0800

    adding metadata and updating abi and safemint function parameters

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,11 +1,15 @@
+import { useState } from "react"
+
 import { ethers } from "ethers"
-import { Box, Button, Flex, Text } from "@chakra-ui/react"
+import { Box, Button, Flex, Text, Input } from "@chakra-ui/react"
 
 import gameNyxNFT from "./GameNyxNFT.json"
 
-const gameNyxNFTAddress = "0xe93b6eeb1DcE633541726c190B3980903049712D"
+const gameNyxNFTAddress = "0x0747dB787BAe464f1FfDBA56475eEA06ee52C107"
 
 export const MainMint = ({ accounts, setAccounts }) => {
+  const [nftUri, setNftUri] = useState("")
+
   const isConnected = Boolean(accounts[0])
 
   async function handleMint() {
@@ -19,7 +23,9 @@ export const MainMint = ({ accounts, setAccounts }) => {
       )
 
       try {
-        const response = await contract.safeMint()
+        const response = await contract.safeMint(accounts[0], nftUri, {
+          value: ethers.utils.parseEther("0.001").toString(),
+        })
         console.log("response: ", response)
       } catch (error) {
         console.log("Error: ", error)
@@ -41,19 +47,35 @@ export const MainMint = ({ accounts, setAccounts }) => {
         {isConnected ? (
           <div>
             <div>
-              <Button
-                backgroundColor="blue"
-                borderRadius="5px"
-                boxShadow="0px 2px 2px 1px #0f0f0f"
-                color="white"
-                cursor="pointer"
-                fontFamily="inherit"
-                padding="15px"
-                margin="0 15px"
-                onClick={handleMint}
-              >
-                Mint my NFT!
-              </Button>
+              <Flex justify={"center"} align="center">
+                <Input
+                  type={"text"}
+                  variant="outline"
+                  size="lg"
+                  placeholder="Enter wallet address"
+                  value={accounts}
+                />
+                <Input
+                  type={"text"}
+                  variant="outline"
+                  size="lg"
+                  placeholder="Enter NFT URI"
+                  onChange={(e) => setNftUri(e.target.value)}
+                />
+                <Button
+                  backgroundColor="blue"
+                  borderRadius="5px"
+                  boxShadow="0px 2px 2px 1px #0f0f0f"
+                  color="white"
+                  cursor="pointer"
+                  fontFamily="inherit"
+                  padding="15px"
+                  margin="0 15px"
+                  onClick={handleMint}
+                >
+                  Mint my NFT!
+                </Button>
+              </Flex>
             </div>
           </div>
         ) : (
